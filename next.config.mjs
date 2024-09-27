6db6da2f1e1f906da2844ae666a136e47289156a
pixelDriftadv <pixeldrift.adv@proton.me>
2024-09-27 13:09:26 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: '',
+        source: '/',
         destination: 'https://advandplv.vercel.app/',
         permanent: true,
       },
